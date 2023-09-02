820d725023d3f16f630f351aecb39e93ea2db03f
SnippyRex <kentf022@gmail.com>
2023-09-02 19:17:39 +0100

    Populate Tip Enum Class

--- app/src/main/java/com/example/a30daysof/data/Tip.kt
+++ app/src/main/java/com/example/a30daysof/data/Tip.kt
@@ -4,11 +4,35 @@ package com.example.a30daysof.data
 import androidx.annotation.StringRes
 import com.example.a30daysof.R
 
-enum class Tip(
-    val number: Int,
-    @StringRes val title: Int,
-    @StringRes val info: Int,
-){
-    One(1, R.string.tip_1_info, R.string.tip_1_info),
-    Two(2, R.string.tip_2_title, R.string.tip_2_info)
+enum class Tip(val number: Int, @StringRes val title: Int, @StringRes val info: Int, ){
+    Tip1(1, R.string.tip_1_title, R.string.tip_1_info),
+    Tip2(2, R.string.tip_2_title, R.string.tip_2_info),
+    Tip3(3, R.string.tip_3_title, R.string.tip_3_info),
+    Tip4(4, R.string.tip_4_title, R.string.tip_4_info),
+    Tip5(5, R.string.tip_5_title, R.string.tip_5_info),
+    Tip6(6, R.string.tip_6_title, R.string.tip_6_info),
+    Tip7(7, R.string.tip_7_title, R.string.tip_7_info),
+    Tip8(8, R.string.tip_8_title, R.string.tip_8_info),
+    Tip9(9, R.string.tip_9_title, R.string.tip_9_info),
+    Tip10(10, R.string.tip_10_title, R.string.tip_10_info),
+    Tip11(11, R.string.tip_11_title, R.string.tip_11_info),
+    Tip12(12, R.string.tip_12_title, R.string.tip_12_info),
+    Tip13(13, R.string.tip_13_title, R.string.tip_13_info),
+    Tip14(14, R.string.tip_14_title, R.string.tip_14_info),
+    Tip15(15, R.string.tip_15_title, R.string.tip_15_info),
+    Tip16(16, R.string.tip_16_title, R.string.tip_16_info),
+    Tip17(17, R.string.tip_17_title, R.string.tip_17_info),
+    Tip18(18, R.string.tip_18_title, R.string.tip_18_info),
+    Tip19(19, R.string.tip_19_title, R.string.tip_19_info),
+    Tip20(20, R.string.tip_20_title, R.string.tip_20_info),
+    Tip21(21, R.string.tip_21_title, R.string.tip_21_info),
+    Tip22(22, R.string.tip_22_title, R.string.tip_22_info),
+    Tip23(23, R.string.tip_23_title, R.string.tip_23_info),
+    Tip24(24, R.string.tip_24_title, R.string.tip_24_info),
+    Tip25(25, R.string.tip_25_title, R.string.tip_25_info),
+    Tip26(26, R.string.tip_26_title, R.string.tip_26_info),
+    Tip27(27, R.string.tip_27_title, R.string.tip_27_info),
+    Tip28(28, R.string.tip_28_title, R.string.tip_28_info),
+    Tip29(29, R.string.tip_29_title, R.string.tip_29_info),
+    Tip30(30, R.string.tip_30_title, R.string.tip_30_info),
 }
