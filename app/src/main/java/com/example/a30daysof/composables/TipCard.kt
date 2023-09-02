4d0e631b9375e54fbe224f8fa87b43b5448ae1ff
SnippyRex <kentf022@gmail.com>
2023-09-02 20:08:59 +0100

    Add styling to TipCard

--- app/src/main/java/com/example/a30daysof/composables/TipCard.kt
+++ app/src/main/java/com/example/a30daysof/composables/TipCard.kt
@@ -1,23 +1,23 @@
 package com.example.a30daysof.composables
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.a30daysof.data.Tip
@@ -25,18 +25,17 @@ import com.example.a30daysof.data.Tip
 @Composable
 fun TipCard(tip: Tip) {
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().wrapContentHeight().padding(horizontal = 4.dp),
         shape = RoundedCornerShape(20.dp),
-        colors = CardDefaults.cardColors(containerColor = Color.Blue)
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer)
     ) {
         Row(
             modifier = Modifier
                 .wrapContentSize()
-                .background(color = Color.Green)
                 .align(Alignment.CenterHorizontally)
         ) {
             Text(text = "Tip: ")
-            Text(text = tip.number.toString())
+            Text(text = tip.number.toString(), color = MaterialTheme.colorScheme.onPrimaryContainer)
         }
         Column(
             modifier = Modifier
@@ -44,11 +43,18 @@ fun TipCard(tip: Tip) {
 
         ) {
             Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-                Text(text = stringResource(id = tip.title))
+                Text(
+                    text = stringResource(id = tip.title),
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    style = MaterialTheme.typography.displaySmall
+                )
             }
             Spacer(modifier = Modifier.size(8.dp))
-            Row(modifier = Modifier.background(Color.Gray)) {
-                Text(text = stringResource(id = tip.info))
+            Row {
+                Text(
+                    text = stringResource(id = tip.info),
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
