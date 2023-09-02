c7cc9518319dd8e929a4e3b29bf8d0f95e8e8d63
SnippyRex <kentf022@gmail.com>
2023-09-02 19:53:59 +0100

    Import Custom Theme

--- app/src/main/java/com/example/a30daysof/MainActivity.kt
+++ app/src/main/java/com/example/a30daysof/MainActivity.kt
@@ -6,19 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.a30daysof.composables.TipCard
 import com.example.a30daysof.data.Tip
-import com.example.a30daysof.ui.theme._30DaysOfTheme
+import com.example.a30daysof.ui.theme.A30DaysOfTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            _30DaysOfTheme {
+            A30DaysOfTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
